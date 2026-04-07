b9c484bbfc554b1c4666182701e0f1ba7321378d
JeRoch4021 <161788973+JeRoch4021@users.noreply.github.com>
2026-04-06 19:52:00 -0600

    Agregando cambios

--- api-constantec/database/fake_data.py
+++ api-constantec/database/fake_data.py
@@ -1,9 +1,9 @@
 from models.factories import AdminFactory, EstudiantesFactory
 
 
-def crear_estudiantes(numero_estudiantes: int):
-    EstudiantesFactory.create_batch(5)
+# def crear_estudiantes(numero_estudiantes: int):
+#     EstudiantesFactory.create_batch(5)
 
 
-def crear_administradores(numero_administradores: int):
-    AdminFactory.create_batch(5)
+# def crear_administradores(numero_administradores: int):
+#     AdminFactory.create_batch(5)
